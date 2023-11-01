ec1a4fe42cee8f936637ddd956a5e5b5ec036717
Tom Hacohen <tom@stosb.com>
2023-10-31 21:09:28 -0700

    Signup: simplify logic by using getOrCreate.

--- src/pages/api/provider/signup.ts
+++ src/pages/api/provider/signup.ts
@@ -15,20 +15,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   // Create Svix application (can also do lazily at a later stage. See docs)
   const svix = new Svix(process.env.SVIX_TOKEN!);
 
-  // REMOVE {
-  // This is just a hack for the example because of the fake auth
-  try {
-    await svix.application.get(data.username);
-    // Application already exists which means a user exists, so let's just return
-    res.json({});
-    return;
-  } catch {
-    // Assume this means an application doesn't exist
-  }
-  // } REMOVE
-
   // Create an application with a uid so that we can refer to it later
-  await svix.application.create({ name: data.company, uid: data.username });
+  // We are using getOrCreate to avoid conflicts in this fake signup flow.
+  await svix.application.getOrCreate({ name: data.company, uid: data.username });
 
   res.json({});
 }
