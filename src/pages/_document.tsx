611e9e360c0066cb1190e6b30826958ae4faf8a5
svix-lucho <lucho@svix.com>
2025-06-26 10:31:45 -0300

    Add base Head in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Svix | example.svix.com</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
