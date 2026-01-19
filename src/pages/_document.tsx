add7912c5d54ad93d4965b52eeede2d22e747db6
svix-lucho <lucho@svix.com>
2026-01-19 10:45:20 -0800

    Fix favicon and make it generic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>Svix | example.svix.com</title>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/icon-32x32.png" sizes="32x32" />
       </Head>
       <body>
         <Main />
